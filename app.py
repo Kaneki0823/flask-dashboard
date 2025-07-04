d05f1e15a55e0cdbf27a61d050769c3ad40bb56e
Kaneki0823 <120104178+Kaneki0823@users.noreply.github.com>
2025-07-04 20:18:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
@@ -16,4 +17,5 @@ def dashboard():
     return render_template("index.html", students=data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render will set PORT automatically
+    app.run(host="0.0.0.0", port=port, debug=False)
